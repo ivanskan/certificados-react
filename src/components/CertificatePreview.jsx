8f325c95a0dd297535231fe5316fac2cce0d55bf
Ivan <ialiagaa@unc.edu.pe>
2025-09-14 11:05:29 -0500

    update design

--- src/components/CertificatePreview.jsx
+++ src/components/CertificatePreview.jsx
@@ -2,42 +2,23 @@ import React from "react";
 
 export default function CertificatePreview({ data, preview = false }) {
   return (
-    <div
-      id={preview ? "certTemplate" : undefined} 
-      className="cert-container row g-0"
-    >
-      {/* Columna izquierda */}
+    <div id={ preview ? "certTemplate" : undefined} className="cert-container row g-0">
+
       <div className="col-2 left-col">
         <img src="/assets/image1.png" className="left-img" alt="decoracion" />
       </div>
 
-      {/* Columna derecha */}
       <div className="col-10 right-col px-5">
-        {/* Marca de agua */}
-        <img src="/assets/marca.png" className="marca" alt="marca agua" />
-
-        {/* Logo superior derecho */}
         <img src="/assets/logo.png" className="logo" alt="logo" />
+        <p className="fs-5 fw-semibold">Otorga el presente certificado a:</p>
+        <p className="fs-3 fw-bold">{data?.["NOMBRE"] || "NOMBRE PARTICIPANTE"}</p>
+        <p className="fs-5">Por haber aprobado el curso de:</p>
+        <p className="fs-4 fw-bold">{data?.["CURSO"] || "NOMBRE DEL CURSO"}</p>
+        <p className="fs-5">{data?.["TEXTO CURSO"] || "Texto curso"}</p>
+        <p className="fs-5 fw-semibold">{data?.["TEXTO FECHA"] || "Fecha y lugar"}</p>
 
-        {/* Contenido textual */}
-        <div className="subtitle">Otorga el presente certificado a:</div>
-        <div className="field name">
-          {data?.["NOMBRE"] || "NOMBRE PARTICIPANTE"}
-        </div>
-        <div className="field">Por haber aprobado el curso de:</div>
-        <div className="field course">
-          {data?.["CURSO"] || "NOMBRE DEL CURSO"}
-        </div>
-        <div className="field">
-          {data?.["TEXTO CURSO"] || "Texto curso"}
-        </div>
-        <div className="field">
-          {data?.["TEXTO FECHA"] || "Fecha y lugar"}
-        </div>
-
-        {/* Bloque de datos extra en dos columnas */}
-        <div className="datos-extra">
-          <div className="col-izq">
+        <div className="datos-extra fs-5">
+          <div className="text-start fw-semibold lh-1">
             <p>Nro. Sesión</p>
             <p>Duración</p>
             <p>Nro. Certificado</p>
@@ -45,35 +26,31 @@ export default function CertificatePreview({ data, preview = false }) {
             <p>Vigencia</p>
             <p>Empresa</p>
           </div>
-          <div className="col-der">
-            <p>: {data?.["NRO SESION"] || "—"}</p>
-            <p>: {data?.["DURACION"] || "—"}</p>
-            <p>: {data?.["NRO CERTIFICADO"] || "—"}</p>
-            <p>: {data?.["MODALIDAD"] || "—"}</p>
-            <p>: {data?.["VIGENCIA"] || "—"}</p>
-            <p>: {data?.["EMPRESA"] || "—"}</p>
+          <div className="text-start lh-1 ms-2">
+            <p>: {data?.["NRO SESION"] || ""}</p>
+            <p>: {data?.["DURACION"] || ""}</p>
+            <p>: {data?.["NRO CERTIFICADO"] || ""}</p>
+            <p>: {data?.["MODALIDAD"] || ""}</p>
+            <p>: {data?.["VIGENCIA"] || ""}</p>
+            <p className="empresa-text over" >: {data?.["EMPRESA"] || ""}</p>
           </div>
         </div>
 
-        {/* Firmas */}
-       <div className="firma-block main-sign-block">
-  <img src="/assets/mainSign.png" className="firma-img" alt="firma gerente" />
-  <div className="firma-line"></div>
-  <div className="firma-nombre">Luisa Narro León</div>
-</div>
+       <div className="firma-block main-sign-block lh-1">
+          <img src="/assets/signatures/Luisa Narro.png" className="firma-img" alt="firma gerente" />
+          <div className="border border-secondary mx-1"></div>
+          <div className="fw-semibold fs-6 pb-1">Luisa Narro León</div>
+          <span>Gerente General</span>
+        </div>
 
-<div className="firma-block instr-sign-block">
-  <img
-    src={`/assets/signatures/${(data?.["INSTRUCTOR"] || "default")
-      .replace(/\s+/g, " ")
-      .trim()}.PNG`}
-    className="firma-img"
-    alt="firma instructor"
-    onError={(e) => (e.target.src = "/assets/signatures/default.PNG")}
-  />
-  <div className="firma-line"></div>
-  <div className="firma-nombre">{data?.["INSTRUCTOR"] || "Instructor"}</div>
-</div>
+        <div className="firma-block instr-sign-block lh-1">
+          <img src={`/assets/signatures/${(data?.["INSTRUCTOR"] || "default").replace(/\s+/g, " ").trim()}.PNG`} className="firma-img"
+            alt="firma instructor" onError={(e) => (e.target.src = "/assets/signatures/default.PNG")}/>
+          <div className="border border-secondary mx-1"></div>
+          <div className="fw-semibold fs-6 pb-1">{data?.["INSTRUCTOR"] ? data["INSTRUCTOR"].toLowerCase().replace(/\b\w/g, (l) => l.toUpperCase())
+              : "Instructor"}</div>
+          <span>Instructor</span>
+        </div>
 
       </div>
     </div>
